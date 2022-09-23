2ecb498771ca07b71e952eb2fd69aaff1262d49a
david <david.lopez.gracia@alumnos.upm.es>
2022-09-23 13:19:23 +0200

    corregido error de cambio de turno en ConnectFour.java #5

--- src/ConnectFour/ConnectFour.java
+++ src/ConnectFour/ConnectFour.java
@@ -15,18 +15,18 @@ public class ConnectFour {
         Board board = new Board();
         Player[] players = {new Player(1), new Player(2)};
         Scanner entrada = new Scanner(System.in);
-        int turn = 1;
+        int turn = 0;
 
         System.out.println("Game start");
         do {
+            turn = changeTurn(turn);
             System.out.println(board.paint());
             System.out.println("Player turn " + players[turn - 1].getColor());
             System.out.println("Type the column where you want to insert the token:");
             board.addToken(Integer.parseInt(entrada.nextLine()), players[turn - 1].getColor());
-            turn = changeTurn(turn);
         } while (!board.hasFourConnected());
         System.out.println(board.paint());
-        System.out.println("Game finished: player" + players[turn - 2].getColor() + " winns");
+        System.out.println("Game finished: player" + players[turn - 1].getColor() + " winns");
     }
 
     private static int changeTurn(int turn) {
