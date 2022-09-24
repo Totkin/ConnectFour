64441e4edb25f1a1bbd3fcc792ec59aa017972e7
david <david.lopez.gracia@alumnos.upm.es>
2022-09-24 13:22:43 +0200

    Comentarios eliminados de ConnectFour.java y metodo renombrado de la clase Player.java

--- src/Player.java
+++ src/Player.java
@@ -11,9 +11,8 @@ public class Player {
         return color;
     }
 
-    public void getUserPosition(Board board){
+    public void addToken(Board board){
         Scanner entrada = new Scanner(System.in);
-        // sntes players[turn - 1].getColor()    ahora this.color
         System.out.println("Player" + this.color + " turn");
         System.out.println("Type the column where you want to insert the token:");
         board.addToken(Integer.parseInt(entrada.nextLine()), this.color);
