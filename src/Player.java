cb64317afef7c14b649e10e163160d303b077ce3
david <david.lopez.gracia@alumnos.upm.es>
2022-09-25 16:03:36 +0200

    Cambio de if for if por un while en addToken de la clase Board.java #19

--- src/Player.java
+++ src/Player.java
@@ -14,7 +14,7 @@ public class Player {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Player " + this.color + " turn");
         System.out.println("Type the column where you want to insert the token:");
-        board.addToken(Integer.parseInt(entrada.nextLine()), this.color);
+        board.addToken(Integer.parseInt(entrada.nextLine())-1, this.color);
     }
 
     public void winnerDisplay() {
