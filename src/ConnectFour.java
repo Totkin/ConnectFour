bd388fb93b50628559f3a562bde10791804d89cb
Taras <taraspodolchak@gmail.com>
2022-09-26 12:18:33 +0200

    add method in class Turn issue #22

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -2,6 +2,7 @@ public class ConnectFour {
     private final int NUMBER_PLAYERS = 2;
     private final Board board = new Board();
     private final Player[] players = new Player[NUMBER_PLAYERS];
+    private final Turn turn=new Turn();
 
     public ConnectFour() {
         for (int i = 0; i < NUMBER_PLAYERS; i++) {
@@ -10,18 +11,13 @@ public class ConnectFour {
     }
 
     private void play() {
-        int turn = 0;
-
         System.out.println("Game start");
         do {
             board.paint();
-            players[turn].addToken(board);
-            if (board.hasFourConnected()) {
-                turn = (++turn) % 2;
-            }
+            players[turn.takeTurn()].addToken(board);
         } while (board.hasFourConnected());
         board.paint();
-        players[turn].winnerDisplay();
+        players[turn.thisTurn()].winnerDisplay();
     }
 
     public static void main(String[] args) {
